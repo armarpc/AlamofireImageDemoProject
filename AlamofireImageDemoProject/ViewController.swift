0c766165d00cab37bd4ed531d4233ae0e34e5199
Carlos Gomez <armarpc@gmail.com>
2021-02-10 12:47:42 -0400

    initial commit

--- AlamofireImageDemoProject/ViewController.swift
+++ AlamofireImageDemoProject/ViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class ViewController: UIViewController {
 
+    var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        
+        let url: URL = URL(string: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png")!
+        imageView.af.setImage(withURL: url)
     }
 
-
 }
-
